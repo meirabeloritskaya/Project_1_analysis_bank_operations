7bdbc04816c7845ffa936504dee5f71967e60a1d
Liudmila Beloritskaya <meiramirth@gmail.com>
2024-06-24 11:46:40 +0300

    fix reports

--- src/reports.py
+++ src/reports.py
@@ -17,66 +17,76 @@ logger.addHandler(file_handler)
 logger.setLevel(logging.INFO)
 
 
-def report_to_file(filename='file_report.txt'):
-    '''Декоратор записывает данные по отчету в файл'''
+def report_to_file(filename="file_report.txt"):
+    """Декоратор записывает данные по отчету в файл"""
+
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             result = func(*args, **kwargs)
-            with open(filename, 'w', encoding='utf-8') as f:
+            with open(filename, "w", encoding="utf-8") as f:
                 f.write(str(result))
-                logger.info('записан результат отчета в файл')
+                logger.info("записан результат отчета в файл")
             return result
+
         return wrapper
+
     return decorator
 
-@report_to_file('file_report.txt')
-def spending_by_category(transactions, category, date=None):
-    '''функция возвращает траты по категориям за последние три месяца'''
 
+@report_to_file("file_report.txt")
+def spending_by_category(transactions, category, date=None):
+    """Функция возвращает траты по категориям за указанный месяц"""
     try:
-        transactions['Дата операции'] = pd.to_datetime(transactions['Дата операции'], format='%d.%m.%Y %H:%M:%S')
-        print(transactions['Дата операции'])
+        # Преобразование столбца 'Дата операции' в формат datetime
+        transactions["Дата операции"] = pd.to_datetime(
+            transactions["Дата операции"], format="%d.%m.%Y %H:%M:%S"
+        )
+
         if date is None:
             end_date = datetime.now()
         else:
-            end_date = datetime.strptime(date, '%d.%m.%Y')
+            end_date = datetime.strptime(date, "%d.%m.%Y")
 
-        start_date = end_date - pd.DateOffset(months=3)
+        # Начало и конец месяца для фильтрации
+        start_date = end_date.replace(day=1)
+        next_month = end_date.replace(day=28) + pd.DateOffset(days=4)
+        end_date = next_month - pd.DateOffset(days=next_month.day)
 
+        # Фильтрация транзакций по дате и категории
+        filtered_transactions = transactions[
+            (transactions["Дата операции"] >= start_date)
+            & (transactions["Дата операции"] <= end_date)
+            & (transactions["Категория"] == category)
+            & (transactions["Сумма платежа"] < 0)
+        ]
 
-        filtered_transactions = transactions[(transactions['Дата операции'] >= start_date) &
-                                             (transactions['Дата операции'] <= end_date) &
-                                             (transactions['Категория'] == category)]
-
+        # Проверка, есть ли отфильтрованные данные
         if filtered_transactions.empty:
             logger.info("Нет данных для указанного периода и категории")
             return []
 
-        grouped_transactions = filtered_transactions.resample('M', on='Дата операции').agg({
-                'Сумма операции': 'sum'
-        }).reset_index()
+        logger.info(f"Траты за указанный месяц по категории {category}")
 
-        logger.info(f'Траты за последние три месяца по категории {category}')
+        print("Данные после фильтрации:")
+        print(filtered_transactions)
 
-        print("Данные после группировки:")
-        print(grouped_transactions)
-
-        return grouped_transactions.to_dict(orient='records')
+        return filtered_transactions.to_dict(orient="records")
 
     except Exception as e:
-          print(f"ошибка {e}")
-          logger.error(f'ошибка {e}')
-          return []
+        print(f"ошибка {e}")
+        logger.error(f"ошибка {e}")
+        return []
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     path = "C:/Users/Meira/PycharmProjects/Project_1_analis_bank_operations/data/operations.xls"
     list_trans = get_data_transactions(path)
 
     transactions = pd.DataFrame(list_trans)
 
-    category = 'Переводы'
-    date = '30.12.2021'
+    category = "Переводы"
+    date = "30.12.2021"
 
     result = spending_by_category(transactions, category, date)
-    print(*result, sep='\n')
+    print(*result, sep="\n")
